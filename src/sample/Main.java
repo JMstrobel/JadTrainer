e15282de380924dc4278f76ff216fd74cc930b27
James Strobel <jmstrobel@gmail.com>
2021-02-12 19:30:01 -0600

    Example animation playing and basic button event handling

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,7 +10,13 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
 
 
 public class Main extends Application {
@@ -18,8 +25,15 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("JadTrainer");
+        Media jadMagic = new Media(new File("resource/JadMagic.mp4").toURI().toString());
+        Media jadRange = new Media(new File("resource/JadRange.mp4").toURI().toString());
 
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        final MediaPlayer animationPlayer = new MediaPlayer(jadMagic);
+        //animationPlayer.setOnEndOfMedia(actionEvent -> {animationPlayer.play();});
+        MediaView animationArea = new MediaView(animationPlayer);
+        animationArea.setFitHeight(224);
+        animationArea.setFitWidth(204);
 
         //Create boxes to hold various components in a layout.
         HBox buttonAnimationAndLogHolder = new HBox();
@@ -27,12 +41,17 @@ public class Main extends Application {
         HBox buttonHolder = new HBox();
 
         //Create the Buttons for user input
-        Image protMagicImg = new Image(getClass().getResourceAsStream("ProtMagic.png"));
-        Image protMisslesImg = new Image(getClass().getResourceAsStream("ProtMissles.png"));
-        Button protMagicButton = new Button("Protection from Magic", new ImageView(protMagicImg));
-        Button protRangeButton = new Button("Protection from Magic", new ImageView(protMisslesImg));
+        Image protMagicImg = new Image(new FileInputStream("resource/ProtMagic.png"));
+        Image protMisslesImg = new Image(new FileInputStream("resource/ProtMissles.png"));
+        Button protMagicButton = new Button("", new ImageView(protMagicImg));
+        Button protRangeButton = new Button("", new ImageView(protMisslesImg));
         Button startStop = new Button("Start!");
 
+        startStop.setOnAction(actionEvent -> {animationPlayer.play();});
+        //Add buttons to appropriate boxes
+        buttonHolder.getChildren().addAll(protMagicButton, protRangeButton, startStop);
+        animationAndButtonHolder.getChildren().addAll(animationArea, buttonHolder);
+        buttonAnimationAndLogHolder.getChildren().addAll(animationAndButtonHolder);
 
         //Initialize scene and bring up the window
         Scene scene = new Scene(buttonAnimationAndLogHolder);
