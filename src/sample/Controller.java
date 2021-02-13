8026e89e5433e13a9383468d03b1d7d0cc97a8c0
James Strobel <jmstrobel@gmail.com>
2021-02-13 03:35:51 -0600

    Command line used for simplest functionality. .mp4 resources need editing

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,84 @@
 package sample;
 
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Controller {
+
+    private boolean playing;
+    private int successCount, failCount, totalDamage;
+    private String activePrayer, targetPrayer;
+
+    public int getSuccessCount() {
+        return successCount;
+    }
+
+    public int getFailCount() {
+        return failCount;
+    }
+
+    Controller(){
+        playing = false;
+        activePrayer = "none";
+        successCount = 0;
+        failCount = 0;
+        totalDamage = 0;
+    }
+    public Controller(String initialPrayer){
+        playing = false;
+        activePrayer = initialPrayer;
+        successCount = 0;
+        failCount = 0;
+        totalDamage = 0;
+    }
+
+    void startStop(MediaPlayer animationPlayer) {
+
+        if(!playing){
+            animationPlayer.play();
+
+        }else{
+            animationPlayer.pause();
+        }
+        playing = !playing;
+    }
+
+    void activateProtMagic(){
+        activePrayer = "magic";
+    }
+    void activateProtMissiles(){
+        activePrayer = "range";
+    }
+
+    String successCheck(String targetPrayer){
+        System.out.print("Target Prayer: " + targetPrayer);
+        if(activePrayer.equals(targetPrayer)) {
+            successCount++;
+            System.out.println(" Prayer chosen successfully. Total successes: " + successCount);
+            return "Prayer set correctly. Good Job!";
+        }else{
+            failCount++;
+            System.out.println(" Failure: " + failCount);
+            successCount = 0;
+            int i = ThreadLocalRandom.current().nextInt(93);
+            return "Jad has hit you. You take: " + i + " damage. \nFor a total of: " + totalDamage + " damage.";
+
+        }
+    }
+
+    String nextAttack() {
+
+        if((int)(Math.random() * 10) % 2 == 0){
+            targetPrayer = "range";
+            return "range";
+        }else{
+           targetPrayer = "magic";
+           return "magic";
+        }
+
+    }
 }
--- src/sample/JadTrainerWindow.java
+++ src/sample/JadTrainerWindow.java
@@ -1,9 +1,6 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -14,26 +11,60 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.File;
 import java.io.FileInputStream;
 
 
-public class Main extends Application {
+public class JadTrainerWindow extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        Controller controller = new Controller();
         primaryStage.setTitle("JadTrainer");
         Media jadMagic = new Media(new File("resource/JadMagic.mp4").toURI().toString());
         Media jadRange = new Media(new File("resource/JadRange.mp4").toURI().toString());
 
 
-        final MediaPlayer animationPlayer = new MediaPlayer(jadMagic);
-        //animationPlayer.setOnEndOfMedia(actionEvent -> {animationPlayer.play();});
-        MediaView animationArea = new MediaView(animationPlayer);
+        final MediaPlayer magicPlayer = new MediaPlayer(jadMagic);
+        final MediaPlayer rangePlayer = new MediaPlayer(jadRange);
+
+
+        MediaView animationArea = new MediaView();
+        if(controller.nextAttack().equals("range")){
+            animationArea.setMediaPlayer(rangePlayer);
+        }else{
+            animationArea.setMediaPlayer(magicPlayer);
+        }
+
         animationArea.setFitHeight(224);
         animationArea.setFitWidth(204);
+
+        magicPlayer.setOnEndOfMedia(() -> {
+            controller.successCheck("magic");
+            magicPlayer.stop();
+            if(controller.nextAttack().equals("range")){
+                animationArea.setMediaPlayer(rangePlayer);
+                rangePlayer.play();
+            }else{
+                animationArea.setMediaPlayer(magicPlayer);
+                magicPlayer.play();
+            }
+        });
+
+        rangePlayer.setOnEndOfMedia(() -> {
+            controller.successCheck("range");
+            rangePlayer.stop();
+            if(controller.nextAttack().equals("range")){
+                animationArea.setMediaPlayer(rangePlayer);
+                rangePlayer.play();
+            }else{
+                animationArea.setMediaPlayer(magicPlayer);
+                magicPlayer.play();
+            }
+        });
 
         //Create boxes to hold various components in a layout.
         HBox buttonAnimationAndLogHolder = new HBox();
@@ -45,9 +76,20 @@ public class Main extends Application {
         Image protMisslesImg = new Image(new FileInputStream("resource/ProtMissles.png"));
         Button protMagicButton = new Button("", new ImageView(protMagicImg));
         Button protRangeButton = new Button("", new ImageView(protMisslesImg));
-        Button startStop = new Button("Start!");
+        Button startStop = new Button("Start");
 
-        startStop.setOnAction(actionEvent -> {animationPlayer.play();});
+
+        startStop.setOnAction(actionEvent -> {
+            controller.startStop(animationArea.getMediaPlayer());
+            if(startStop.getText().equals("Start")){
+                startStop.setText("Stop");
+            }else{
+                startStop.setText("Start");
+            }
+        });
+        protMagicButton.setOnAction(actionEvent -> {controller.activateProtMagic();});
+        protRangeButton.setOnAction(actionEvent -> {controller.activateProtMissiles();});
+
         //Add buttons to appropriate boxes
         buttonHolder.getChildren().addAll(protMagicButton, protRangeButton, startStop);
         animationAndButtonHolder.getChildren().addAll(animationArea, buttonHolder);
